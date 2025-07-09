f0201bc6ed35f175b577bd4e84b422414d09ee1f
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2025-07-09 14:10:44 +0600

    css conflict added

--- templates/assets/script.js
+++ templates/assets/script.js
@@ -4,10 +4,10 @@ const toggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
 toggle.addEventListener("click", () => {
-  console.log("Menu toggle clicked");
   if (mobileMenu.classList.contains("hidden")) {
     mobileMenu.classList.remove("hidden");
     mobileMenu.classList.add("animate-slide-down");
+    mobileMenu.classList.add("flex");
 
     // Remove animation class after it plays so it can replay next time
     setTimeout(() => {
