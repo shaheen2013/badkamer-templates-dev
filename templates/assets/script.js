5db669535f86e95041f56a989af736fa01b21935
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2025-07-09 14:07:27 +0600

    Responsive mobile header created

--- templates/assets/script.js
+++ templates/assets/script.js
@@ -1,1 +1,19 @@
 console.log("Script loaded successfully!");
+
+const toggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+toggle.addEventListener("click", () => {
+  console.log("Menu toggle clicked");
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+    mobileMenu.classList.add("animate-slide-down");
+
+    // Remove animation class after it plays so it can replay next time
+    setTimeout(() => {
+      mobileMenu.classList.remove("animate-slide-down");
+    }, 300);
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+});
